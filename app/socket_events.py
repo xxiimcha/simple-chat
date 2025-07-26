6c0eb25aa08c52c315cac4a25d53b23f3fe01c4c
xxiimcha <charmaine.l.d.cator@gmail.com>
2025-07-26 10:39:58 +0800

    Socket IO implemented

--- app/socket_events.py
+++ app/socket_events.py
@@ -1,17 +1,34 @@
 from flask_socketio import emit
-from . import socketio
 from flask_login import current_user
-from .models import db, Message
-from datetime import datetime
+from datetime import datetime, timedelta
+from .models import Message, User
+from . import db, socketio
 
-@socketio.on('send_message')
-def handle_send(data):
-    content = data['content']
-    msg = Message(content=content, user_id=current_user.id, timestamp=datetime.utcnow())
-    db.session.add(msg)
+@socketio.on("send_message")
+def handle_send_message(data):
+    content = data.get("content", "")
+    recipient_id = data.get("recipient_id")
+
+    message = Message(
+        user_id=current_user.id,
+        content=content,
+        recipient_id=recipient_id if recipient_id else None
+    )
+    db.session.add(message)
     db.session.commit()
-    emit('receive_message', {
-        'username': current_user.username,
-        'content': msg.content,
-        'timestamp': msg.timestamp.strftime('%Y-%m-%d %H:%M')
-    }, broadcast=True)
+
+    recipient_user = User.query.get(recipient_id) if recipient_id else None
+
+    response = {
+        "content": content,
+        "sender_username": current_user.username,
+        "recipient_username": recipient_user.username if recipient_user else None,
+        "timestamp": (datetime.utcnow() + timedelta(hours=8)).strftime('%Y-%m-%d %H:%M'),
+        "self": True
+    }
+
+    emit("receive_message", response)
+
+    response["self"] = False
+
+    emit("receive_message", response, broadcast=True, include_self=False)
--- run.py
+++ run.py
@@ -2,5 +2,5 @@ from app import create_app, socketio
 
 app = create_app()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     socketio.run(app, debug=True)
